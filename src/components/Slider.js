bf2423a45d58239c609fa85807150270084c7d83
ogc1231 <ogc@live.ie>
2023-09-13 17:11:02 +0000

    Redisgn slider layout

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -26,144 +26,144 @@ function Slider() {
   return (
     <Carousel className={styles.Slider}>
 
-      <Carousel.Item interval={2000}>
-      <img src={ar} alt="ar"/>
-        <Carousel.Caption>
-          <h3>Argentina</h3>
-        </Carousel.Caption>
+      <Carousel.Item interval={3000}>
+      <div className={styles.Flag}>
+        <img src={ar} alt="ar"/>
+        <h3 className={styles.FlagFont}>Argentina</h3>
+      </div>
       </Carousel.Item>
 
-      <Carousel.Item interval={2000}>
-      <img src={bo} alt="ar"/>
-        <Carousel.Caption>
-          <h3>Bolvia</h3>
-        </Carousel.Caption>
+      <Carousel.Item interval={3000}>
+      <div className={styles.Flag}>
+        <img src={bo} alt="bolivia"/>
+        <h3 className={styles.FlagFont}>Bolivia</h3>
+      </div>
       </Carousel.Item>
 
-      <Carousel.Item interval={2000}>
-      <img src={ch} alt="ar"/>
-        <Carousel.Caption>
-          <h3>Chile</h3>
-        </Carousel.Caption>
+      <Carousel.Item interval={3000}>
+      <div className={styles.Flag}>
+        <img src={ch} alt="chile"/>
+        <h3 className={styles.FlagFont}>Chile</h3>
+      </div>
       </Carousel.Item>
 
-      <Carousel.Item interval={2000}>
-      <img src={co} alt="ar"/>
-        <Carousel.Caption>
-          <h3>Colombia</h3>
-        </Carousel.Caption>
+      <Carousel.Item interval={3000}>
+      <div className={styles.Flag}>
+        <img src={co} alt="colombia"/>
+        <h3 className={styles.FlagFont}>Colombia</h3>
+      </div>
       </Carousel.Item>
 
-      <Carousel.Item interval={2000}>
-      <img src={cr} alt="ar"/>
-        <Carousel.Caption>
-          <h3 className={styles.Textb}>Costa Rica</h3>
-        </Carousel.Caption>
+      <Carousel.Item interval={3000}>
+      <div className={styles.Flag}>
+        <img src={cr} alt="costa rica"/>
+        <h3 className={styles.FlagFont}>Costa Rica</h3>
+      </div>
       </Carousel.Item>
 
-      <Carousel.Item interval={2000}>
-      <img src={cu} alt="ar"/>
-        <Carousel.Caption>
-          <h3 className={styles.Text}>Cuba</h3>
-        </Carousel.Caption>
+      <Carousel.Item interval={3000}>
+      <div className={styles.Flag}>
+        <img src={cu} alt="cuba"/>
+        <h3 className={styles.FlagFont}>Cuba</h3>
+      </div>
       </Carousel.Item>
 
-      <Carousel.Item interval={2000}>
-      <img src={dr} alt="ar"/>
-        <Carousel.Caption>
-          <h3>Dominican Republic</h3>
-        </Carousel.Caption>
+      <Carousel.Item interval={3000}>
+      <div className={styles.Flag}>
+        <img src={dr} alt="dominican republic"/>
+        <h3 className={styles.FlagFont}>Dominican Republic</h3>
+      </div>
       </Carousel.Item>
 
-      <Carousel.Item interval={2000}>
-      <img src={ec} alt="ar"/>
-        <Carousel.Caption>
-          <h3>Ecuador</h3>
-        </Carousel.Caption>
+      <Carousel.Item interval={3000}>
+      <div className={styles.Flag}>
+        <img src={ec} alt="ecuador"/>
+        <h3 className={styles.FlagFont}>Ecuador</h3>
+      </div>
       </Carousel.Item>
 
-      <Carousel.Item interval={2000}>
-      <img src={el} alt="ar"/>
-        <Carousel.Caption>
-          <h3>El Salvador</h3>
-        </Carousel.Caption>
+      <Carousel.Item interval={3000}>
+      <div className={styles.Flag}>
+        <img src={el} alt="el salvador"/>
+        <h3 className={styles.FlagFont}>El Salvador</h3>
+      </div>
       </Carousel.Item>
 
-      <Carousel.Item interval={2000}>
-      <img src={eg} alt="ar"/>
-        <Carousel.Caption>
-          <h3>Equatorial Guinea</h3>
-        </Carousel.Caption>
+      <Carousel.Item interval={3000}>
+      <div className={styles.Flag}>
+        <img src={eg} alt="equatorial guinea"/>
+        <h3 className={styles.FlagFont}>Equatorial Guinea</h3>
+      </div>
       </Carousel.Item>
 
-      <Carousel.Item interval={2000}>
-      <img src={gu} alt="ar"/>
-        <Carousel.Caption>
-          <h3 className={styles.Textb}>Guatemala</h3>
-        </Carousel.Caption>
+      <Carousel.Item interval={3000}>
+      <div className={styles.Flag}>
+        <img src={gu} alt="guatemala"/>
+        <h3 className={styles.FlagFont}>Guatemala</h3>
+      </div>
       </Carousel.Item>
 
-      <Carousel.Item interval={2000}>
-      <img src={ho} alt="ar"/>
-        <Carousel.Caption>
-          <h3>Honduras</h3>
-        </Carousel.Caption>
+      <Carousel.Item interval={3000}>
+      <div className={styles.Flag}>
+        <img src={ho} alt="honduras"/>
+        <h3 className={styles.FlagFont}>Honduras</h3>
+      </div>
       </Carousel.Item>
 
-      <Carousel.Item interval={2000}>
-      <img src={me} alt="ar"/>
-        <Carousel.Caption>
-          <h3 className={styles.Textb}>Mexico</h3>
-        </Carousel.Caption>
+      <Carousel.Item interval={3000}>
+      <div className={styles.Flag}>
+        <img src={me} alt="mexico"/>
+        <h3 className={styles.FlagFont}>Mexico</h3>
+      </div>
       </Carousel.Item>
 
-      <Carousel.Item interval={2000}>
-      <img src={ni} alt="ar"/>
-        <Carousel.Caption>
-          <h3>Nicaragua</h3>
-        </Carousel.Caption>
+      <Carousel.Item interval={3000}>
+      <div className={styles.Flag}>
+        <img src={ni} alt="nicaragua"/>
+        <h3 className={styles.FlagFont}>Nicaragua</h3>
+      </div>
       </Carousel.Item>
 
-      <Carousel.Item interval={2000}>
-      <img src={pa} alt="ar"/>
-        <Carousel.Caption>
-          <h3>Panama</h3>
-        </Carousel.Caption>
+      <Carousel.Item interval={3000}>
+      <div className={styles.Flag}>
+        <img src={pa} alt="panama"/>
+        <h3 className={styles.FlagFont}>Panama</h3>
+      </div>
       </Carousel.Item>
 
-      <Carousel.Item interval={2000}>
-      <img src={pr} alt="ar"/>
-        <Carousel.Caption>
-          <h3>Paraguay</h3>
-        </Carousel.Caption>
+      <Carousel.Item interval={3000}>
+      <div className={styles.Flag}>
+        <img src={pr} alt="paraguay"/>
+        <h3 className={styles.FlagFont}>Paraguay</h3>
+      </div>
       </Carousel.Item>
 
-      <Carousel.Item interval={2000}>
-      <img src={pe} alt="ar"/>
-        <Carousel.Caption>
-          <h3 className={styles.Textb}>Peru</h3>
-        </Carousel.Caption>
+      <Carousel.Item interval={3000}>
+      <div className={styles.Flag}>
+        <img src={pe} alt="peru"/>
+        <h3 className={styles.FlagFont}>Peru</h3>
+      </div>
       </Carousel.Item>
 
-      <Carousel.Item interval={2000}>
-      <img src={es} alt="ar"/>
-        <Carousel.Caption>
-          <h3>Spain</h3>
-        </Carousel.Caption>
+      <Carousel.Item interval={3000}>
+      <div className={styles.Flag}>
+        <img src={es} alt="spain"/>
+        <h3 className={styles.FlagFont}>Spain</h3>
+      </div>
       </Carousel.Item>
 
-      <Carousel.Item interval={2000}>
-      <img src={ur} alt="ar"/>
-        <Carousel.Caption>
-          <h3 className={styles.Text}>Uruguay</h3>
-        </Carousel.Caption>
+      <Carousel.Item interval={3000}>
+      <div className={styles.Flag}>
+        <img src={ur} alt="uruguay"/>
+        <h3 className={styles.FlagFont}>Uruguay</h3>
+      </div>
       </Carousel.Item>
 
-      <Carousel.Item interval={2000}>
-      <img src={ve} alt="ar"/>
-        <Carousel.Caption>
-          <h3>Venezuela</h3>
-        </Carousel.Caption>
+      <Carousel.Item interval={3000}>
+      <div className={styles.Flag}>
+        <img src={ve} alt="venezuela"/>
+        <h3 className={styles.FlagFont}>Venezuela</h3>
+      </div>
       </Carousel.Item>
 
     </Carousel>
