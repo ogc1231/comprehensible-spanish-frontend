7a06c0e20e9ed6e6fb60f3ad4a1a224b6bc71b8d
ogc1231 <ogc@live.ie>
2023-09-13 13:57:01 +0000

    Undo changes and delete unused files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
-import Container from "react-bootstrap/Container";
 import Slider from "./components/Slider";
+import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
-// import "./api/axiosDefaults";
-import SignUpForm from "./pages/auth/SignUpForm";
 
 function App() {
   return (
@@ -14,7 +12,7 @@ function App() {
         <Switch>
           <Route exact path="/" render={() => <Slider />} />
           <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
-          <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
--- src/api/axiosDefault.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-axios.defaults.baseURL = "ADD LINK HERE TO DATABASE";
-axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
-axios.defaults.withCredentials = true;
-
-export const axiosReq = axios.create();
-export const axiosRes = axios.create();
