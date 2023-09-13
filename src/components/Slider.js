ae33c2dca571d34d2cd7d8bc2e5a9b1918e5fc3f
ogc1231 <ogc@live.ie>
2023-09-13 12:30:17 +0000

    Upload flag images

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -7,24 +7,28 @@ import ch from "../assets/ch.png";
 function Slider() {
   return (
     <Carousel className={styles.Slider}>
+        
       <Carousel.Item interval={2000}>
       <img src={ar} alt="ar"/>
         <Carousel.Caption>
           <h3>Argentina</h3>
         </Carousel.Caption>
       </Carousel.Item>
+
       <Carousel.Item interval={2000}>
       <img src={bo} alt="ar"/>
         <Carousel.Caption>
           <h3>Bolvia</h3>
         </Carousel.Caption>
       </Carousel.Item>
+
       <Carousel.Item interval={2000}>
       <img src={ch} alt="ar"/>
         <Carousel.Caption>
           <h3>Chile</h3>
         </Carousel.Caption>
       </Carousel.Item>
+
     </Carousel>
   );
 }
