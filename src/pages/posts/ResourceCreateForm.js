a21d32bcc314fec0e6273e28711e31999e3221d2
ogc1231 <ogc@live.ie>
2023-09-15 12:38:05 +0000

    Add ipload image functionality to create resource

--- src/pages/posts/ResourceCreateForm.js
+++ src/pages/posts/ResourceCreateForm.js
@@ -11,18 +11,60 @@ import Upload from "../../assets/upload.png";
 import styles from "../../styles/ResourceCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
+import Asset from "../../components/Asset";
+import { Image } from "react-bootstrap";
 
 function ResourceCreateForm() {
 
   const [errors, setErrors] = useState({});
 
+  const [resourceData, setResourceData] = useState({
+    title: "",
+    content: "",
+    image: "",
+  });
+  const { title, description, image } = resourceData;
+
+  const handleChange = (event) => {
+    setResourceData({
+      ...resourceData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleChangeImage = (event) => {
+    if (event.target.files.length) {
+      URL.revokeObjectURL(image);
+      setResourceData({
+        ...resourceData,
+        image: URL.createObjectURL(event.target.files[0]),
+      });
+    }
+  };
+
 
   const textFields = (
     <div className="text-center">
-      {/* Add your form fields here */}
-
-    
-    
+      <Form.Group>
+        <Form.Label>Title</Form.Label>
+        <Form.Control
+          type="text"
+          name="title"
+          value={title}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Description</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows={6}
+          name="description"
+          value={description}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      
       <Button
         className={`${btnStyles.Button} ${btnStyles.Blue}`}
         onClick={() => {}}
@@ -43,14 +85,37 @@ function ResourceCreateForm() {
             className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
           >
             <Form.Group className="text-center">
-              
-                <Form.Label
+                {image ? (
+                  <>
+                  <figure>
+                    <Image className={appStyles.Image} src={image} rounded />
+                  </figure>
+                  <div>
+                    <Form.Label
+                      className={`${btnStyles.Button} ${btnStyles.Blue} btn`}
+                      htmlFor="image-upload"
+                    >
+                      Change the image
+                    </Form.Label>
+                  </div>
+                </>
+                ) : (
+                  <Form.Label
                   className="d-flex justify-content-center"
                   htmlFor="image-upload"
                 >
-                  ASSET
+                  <Asset
+                    src={Upload}
+                    message="Click or tap to upload an image"
+                  />
                 </Form.Label>
-
+                )}
+                
+                <Form.File
+                  id="image-upload"
+                  accept="image/*"
+                  onChange={handleChangeImage}
+                />
             </Form.Group>
             <div className="d-md-none">{textFields}</div>
           </Container>
