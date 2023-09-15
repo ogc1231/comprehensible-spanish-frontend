e98b360eaa9c0cbbea3a73b994ccf8c4a056c3a8
ogc1231 <ogc@live.ie>
2023-09-15 11:43:05 +0000

    Add addRes, profile, signout & favourites to nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,11 +4,45 @@ import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
 import { useCurrentUser } from "../contexts/CurrentUserContext";
+import Avatar from "./Avatar";
+import axios from "axios";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
 
-  const loggedInIcons = <>{currentUser?.username}</>;
+  const addResourceIcon = (
+    <NavLink
+      className={styles.NavLink}
+      activeClassName={styles.Active}
+      to="/resources/create"
+    >
+      <i className="far fa-plus-square"></i>Add resource
+    </NavLink>
+  );
+
+  const loggedInIcons = <>
+    <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/resources"
+      >
+        <i className="fas fa-stream"></i>Feed
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/favourites"
+      >
+        <i className="fas fa-heart"></i>Favourites
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        to={`/profiles/${currentUser?.profile_id}`}
+      >
+        <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
+      </NavLink>
+  </>;
+
   const loggedOutIcons = (
     <>
       <NavLink
@@ -39,6 +73,7 @@ const NavBar = () => {
             </div>
           </Navbar.Brand>
         </NavLink>
+        {currentUser && addResourceIcon}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
