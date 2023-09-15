1ed20d51e9f9aded7f064cee6b01118ea5627f40
ogc1231 <ogc@live.ie>
2023-09-15 11:29:06 +0000

    Add axiosReq & axiosRes interceptor for auth token

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://comp-spanish-api-35f1e1574e0f.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
