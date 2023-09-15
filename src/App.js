1a0e64435e03091eea65d796e3b505354f827f04
ogc1231 <ogc@live.ie>
2023-09-15 11:01:43 +0000

    Create signin form with authentication

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForm";
 
 function App() {
   return (
@@ -20,7 +21,7 @@ function App() {
             <UnifiedDropdown />
           </div>
            } />
-          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+          <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
