90929c8b5181c40f6ce121d7f005794371f8758f
ogc1231 <ogc@live.ie>
2023-09-17 17:55:33 +0000

    Create filter dropdowns

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
-import Slider from "./components/Slider";
+import FilterDropdown from "./components/FilterDropdown";
 
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
@@ -26,7 +26,7 @@ function App() {
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() =><Slider />} />
+          <Route exact path="/" render={() =><FilterDropdown />} />
           <Route exact path="/resources" render={() => <ResourcesPage message="No results found. Adjust the search keyword."/>} />
           <Route
             exact
---
+++ src/components/FilterDropdown.js
@@ -0,0 +1,91 @@
+import React from "react";
+import Dropdown from 'react-bootstrap/Dropdown';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import styles from "../styles/FilterDropdown.module.css";
+
+function LevelDropdown() {
+    return (
+      <Dropdown className={styles.FilterDesign}>
+        <Dropdown.Toggle variant="success" id="dropdown-basic" className={styles.FilterDesign}>
+        <i className="fa-solid fa-signal"></i>Level
+        </Dropdown.Toggle>
+  
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-1">Learner</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Easy Native</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Native</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+    );
+  }
+
+function ResourceTypeDropdown() {
+  return (
+    <Dropdown className={styles.FilterDesign}>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+      <i className="fa-solid fa-headphones"></i>Resource Type
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">YouTube - Video</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Podcast - Audio</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
+
+function CountryDropdown() {
+  return (
+    <Dropdown className={styles.FilterDesign}>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+      <i className="fa-solid fa-earth-americas"></i>Country
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu className={styles.Menu}>
+        <Dropdown.Item href="#/action-0">Mixed</Dropdown.Item>
+        <Dropdown.Item href="#/action-1">Argentina</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Bolivia</Dropdown.Item>
+        <Dropdown.Item href="#/action-ca">Canary Islands</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Chile</Dropdown.Item>
+        <Dropdown.Item href="#/action-4">Colombia</Dropdown.Item>
+        <Dropdown.Item href="#/action-5">Costa Rica</Dropdown.Item>
+        <Dropdown.Item href="#/action-6">Cuba</Dropdown.Item>
+        <Dropdown.Item href="#/action-7">Dominican Republic</Dropdown.Item>
+        <Dropdown.Item href="#/action-8">Ecuador</Dropdown.Item>
+        <Dropdown.Item href="#/action-9">El Salvador</Dropdown.Item>
+        <Dropdown.Item href="#/action-10">Equatorial Guinea</Dropdown.Item>
+        <Dropdown.Item href="#/action-11">Guatemala</Dropdown.Item>
+        <Dropdown.Item href="#/action-12">Honduras</Dropdown.Item>
+        <Dropdown.Item href="#/action-13">Mexico</Dropdown.Item>
+        <Dropdown.Item href="#/action-14">Nicaragua</Dropdown.Item>
+        <Dropdown.Item href="#/action-15">Panama</Dropdown.Item>
+        <Dropdown.Item href="#/action-16">Paraguay</Dropdown.Item>
+        <Dropdown.Item href="#/action-17">Peru</Dropdown.Item>
+        <Dropdown.Item href="#/action-18">Spain</Dropdown.Item>
+        <Dropdown.Item href="#/action-19">Uruguay</Dropdown.Item>
+        <Dropdown.Item href="#/action-20">Venezuela</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
+
+export default function FilterDropdown() {
+    return(
+        <div>
+            <Navbar expand="lg" className="bg-body-tertiary">
+              <Container>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                  <Nav className="me-auto">
+                  <LevelDropdown />
+                  <ResourceTypeDropdown />
+                  <CountryDropdown />
+                  </Nav>
+                </Navbar.Collapse>
+              </Container>
+            </Navbar>
+        </div>
+    )
+}
