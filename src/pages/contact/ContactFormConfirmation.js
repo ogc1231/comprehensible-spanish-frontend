346c8cffed2b56edabd9b38cfcb1e1169f760152
ogc1231 <ogc@live.ie>
2023-09-21 14:37:29 +0000

    Add 5 second redirect to home page

--- src/pages/contact/ContactFormConfirmation.js
+++ src/pages/contact/ContactFormConfirmation.js
@@ -1,24 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import styles from "../../styles/ContactFormConfirmation.module.css";
 import appStyles from "../../App.module.css";
-import { useRedirect } from "../../hooks/useRedirect";
 
 import {
   Col,
   Row,
   Container,
 } from "react-bootstrap";
+import { useRedirect } from "../../hooks/useRedirect";
 
 
 const ContactFormConfirmation = () => {
   useRedirect("loggedOut");
+  const [count, setCount] = useState(5);
+  const navigate = useHistory();
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // update the state after 1000ms
+      setCount((currentCount) => currentCount - 1);
+    }, 1000);
+    // when count is 0, navigate
+    count === 0 && navigate.push("/");
+    // clean up the interval
+    return () => clearInterval(interval);
+  }, [count, navigate]);
+
   return (    
     <Row className={styles.Row}>
-      
       <Col>
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>Thank you</h1>
-          <p className="text-center">We have received your message and will be in touch soon!</p>    
+          <p className="text-center">We have received your message and will be in touch soon!</p>   
+          <p className="text-center">Redirecting you in {count} sec </p> 
         </Container>
       </Col>
     </Row>
