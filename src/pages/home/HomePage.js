ffdf4b6c023a4c143543b6ba518b48e85435d8a8
ogc1231 <ogc@live.ie>
2023-09-22 13:19:17 +0000

    Add hero image to HomePage.js

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import styles from "../../styles/HomePage.module.css";
+import hero from "../../assets/hero.jpg"
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className={styles.HeroDiv}>
+      <img src={hero} alt='hero' className={styles.HeroImage}/>
+    </div>
   )
 }
 
