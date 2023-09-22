116c6247721b3237b5016dc4a6239bdfaa58a172
ogc1231 <ogc@live.ie>
2023-09-22 10:26:13 +0000

    Change navbar collapse point

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -115,7 +115,7 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar expanded={expanded} className={styles.NavBar} expand="md" fixed="top">
+    <Navbar expanded={expanded} className={styles.NavBar} expand="lg" fixed="top">
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
