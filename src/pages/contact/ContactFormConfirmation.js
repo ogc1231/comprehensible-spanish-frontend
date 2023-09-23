8af4e61a7afb5c3b231a9b284943ac6cb7c5455c
ogc1231 <ogc@live.ie>
2023-09-23 13:20:16 +0000

    Style contact & confirmation forms

--- src/pages/contact/ContactFormConfirmation.js
+++ src/pages/contact/ContactFormConfirmation.js
@@ -10,7 +10,7 @@ import { useRedirect } from "../../hooks/useRedirect";
 
 const ContactFormConfirmation = () => {
   useRedirect("loggedOut");
-  const [count, setCount] = useState(5);
+  const [count, setCount] = useState(10);
   const navigate = useHistory();
   useEffect(() => {
     const interval = setInterval(() => {
@@ -24,9 +24,9 @@ const ContactFormConfirmation = () => {
     <Row className={styles.Row}>
       <Col>
         <Container className={`${appStyles.Content} p-4 `}>
-          <h1 className={styles.Header}>Thank you</h1>
+          <h2 className={styles.Header}>Thank you for contacting us</h2>
           <p className="text-center">We have received your message and will be in touch soon!</p>   
-          <p className="text-center">Redirecting you in {count} sec </p> 
+          <p className="text-center">Redirecting you in {count} seconds </p> 
         </Container>
       </Col>
     </Row>
