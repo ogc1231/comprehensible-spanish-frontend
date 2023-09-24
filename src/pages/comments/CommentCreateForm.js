1350931fa4c6ed69afbf1d223757d4e94736b359
ogc1231 <ogc@live.ie>
2023-09-24 08:25:39 +0000

    Style post button

--- src/pages/comments/CommentCreateForm.js
+++ src/pages/comments/CommentCreateForm.js
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
+import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 
 import styles from "../../styles/CommentCreateEditForm.module.css";
 import Avatar from "../../components/Avatar";
 import { axiosRes } from "../../api/axiosDefaults";
+import Alert from "react-bootstrap/Alert";
+import btnStyles from "../../styles/Button.module.css";
 
 function CommentCreateForm(props) {
   const { resource, setResource, setComments, profileImage, profile_id } = props;
   const [content, setContent] = useState("");
+  const [errors, setErrors] = useState({});
 
   const handleChange = (event) => {
     setContent(event.target.value);
@@ -58,13 +61,17 @@ function CommentCreateForm(props) {
           />
         </InputGroup>
       </Form.Group>
-      <button
-        className={`${styles.Button} btn d-block ml-auto`}
-        disabled={!content.trim()}
+      {errors?.content?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+      <Button
+        className={`${styles.Button} ${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright} btn d-block ml-auto`}
         type="submit"
       >
         Post
-      </button>
+      </Button>
     </Form>
   );
 }
